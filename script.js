6d9bed1223e2f82b2c670071256f88930619df10
Annestezia <cast.wheel@gmail.com>
2020-07-02 19:52:37 +1000

    added click handler, need more fixes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
   window.onload = (function(){
     const s = document.querySelectorAll('.wp');    
     const links = document.querySelectorAll('.nav-link');
+    const navBar = document.getElementById('nav-bar');
+    
 
 
     const setActive = el => {
@@ -8,14 +10,26 @@
       el.classList.add('active');
       };
 
-
+      //for scroll
       for(let i = 0; i<s.length;i++){
         new Waypoint({
           element: s[i],
           handler: function(direction){
             setActive(links[i]);
           },
-          offset: 'bottom-in-view' 
+          offset:function () {
+            return this.context.innerHeight() - this.adapter.outerHeight() - 100;
+            }  
         });
       }
+      // for click
+     
+      navBar.addEventListener('click',(e)=>{
+        if(!e.target.tagName==="A"){
+          return;
+        }
+        e.target.addClass('active');
+       
+      })
   })(); 
+ 
